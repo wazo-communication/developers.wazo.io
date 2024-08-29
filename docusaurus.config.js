d698ccb89684cc4e24138deb6f54d1f9da6a4ac0
Francis Chartrand <chartrandf@users.noreply.github.com>
2024-08-29 09:32:40 -0400

    build: add mermaid

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,11 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -130,7 +135,12 @@ const config = {
         darkTheme: darkCodeTheme,
         additionalLanguages: ['bash', 'diff', 'json', 'javascript', 'typescript', 'python'],
       },
+
+      mermaid: {
+        theme: {light: 'neutral', dark: 'dark'},
+      }
     }),
+
   clientModules: [
     require.resolve('./src/softphone-module.js'),
   ],
